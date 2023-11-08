9929276b054866694d51f77b3e7a5f446bf2d8f7
Sunil Thakur <sunilthakur178co@gmail.com>
2023-11-08 17:31:25 +0530

    Fixed recruiter dashboard button link

--- resources/views/recruiter/dashboard.blade.php
+++ resources/views/recruiter/dashboard.blade.php
@@ -7,11 +7,11 @@
 
     <div class="py-12">
         <div class="flex flex-wrap gap-3 items-center justify-center">
-            <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-1/4">
+            <a href="{{route('job.index')}}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-1/4">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $jobs }}</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">Total Jobs Created</p>
             </a>
-            <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-1/4">
+            <a href="{{route('job.index')}}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-1/4">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $candidate_jobs }}</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">Total Applications Received</p>
             </a>
